e7e30eca406197fa4477625a80107a2b1ebdce96
Jose Herrera <jooherrera4@gmail.com>
2022-05-01 02:24:18 -0300

    Header Finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,6 @@ import { BtnLogComponent } from './components/navbar/btn-log/btn-log.component';
 import { BtnRedSocialComponent } from './components/navbar/btn-red-social/btn-red-social.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ProfileImgComponent } from './components/header/profile-img/profile-img.component';
-import { ProfileInfoComponent } from './components/header/profile-info/profile-info.component';
-import { BannerComponent } from './components/header/banner/banner.component';
 
 @NgModule({
   declarations: [
@@ -19,16 +16,9 @@ import { BannerComponent } from './components/header/banner/banner.component';
     BtnLogComponent,
     BtnRedSocialComponent,
     HeaderComponent,
-    ProfileImgComponent,
-    ProfileInfoComponent,
-    BannerComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
